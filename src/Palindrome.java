911061e739a19587b9fdb336b95a09c8f9f02e0f
ushpreetsingh.chadha <ushpreetsingh.chadha@DEL1-DHP-28226.synapse.com>
2016-12-23 12:05:06 +0530

    Changed the class code

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,8 +1,8 @@
 
 public class Palindrome{  
 	
-	 public static void main(String args[]){  
-		 
+	public static void PalindromeExample()
+	{
 		  int r,sum=0,temp;    
 		  int n=645;
 		  
@@ -18,5 +18,27 @@ public class Palindrome{
 		   System.out.println("The number is a palindrome ");    
 		  else    
 		   System.out.println("The number is not a palindrome ");    
-		}  
+		
+	}
+	
+	public static void HelloWorld()
+	{
+		int[] numbers = new int[]{1,2};
+	       
+        for(int i=0; i < numbers.length; i++){
+        	if(numbers[i]%2 == 0)
+                System.out.println(numbers[i] + " is even number.");
+              else
+                System.out.println(numbers[i] + " is odd number.");
+              
+                       
+        }
+	}
+	
+	 public static void main(String args[]){  
+		 
+		  Palindrome pal1 = new Palindrome();
+		  pal1.PalindromeExample();
+		  pal1.HelloWorld();
+	 }
 		} 
